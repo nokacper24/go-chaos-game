c9f1471e470d122cd818b89647487e683302d0e2
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2024-06-11 20:39:07 +0200

    feat: implement chaos game

---
+++ internal/chaosgame/game/chaos_game.go
@@ -0,0 +1,30 @@
+package game
+
+import (
+	"math/rand"
+
+	"github.com/nokacper24/go-chaos-game/internal/chaosgame/model"
+)
+
+type ChaosGame struct {
+	description  *ChaosGameDescription
+	currentPoint *model.Vector2D
+	Canvas       *chaosCanvas
+}
+
+func NewChaosGame(description *ChaosGameDescription, width int, height int) *ChaosGame {
+	return &ChaosGame{
+		description:  description,
+		currentPoint: &model.Vector2D{X: 0, Y: 0},
+		Canvas:       NewChaosCanvas(width, height, model.Vector2D{X: 0, Y: 0}, model.Vector2D{X: 1, Y: 1}),
+	}
+}
+
+func (cg *ChaosGame) RunSteps(steps int) {
+	numOfTransforms := len(cg.description.Transforms)
+	for range steps {
+		transform := cg.description.Transforms[rand.Intn(numOfTransforms)]
+		cg.currentPoint = transform.Transform(cg.currentPoint)
+		cg.Canvas.PutPixel(*cg.currentPoint)
+	}
+}
