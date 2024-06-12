56dfb8f240c5557bb60ad49f9ae83788fb06019f
Kacper Lukasz Nowicki <nokacper24@gmail.com>
2024-06-12 13:32:23 +0200

    feat: add barnsley fern and a julia transformtion

--- internal/chaosgame/game/chaos_game_description.go
+++ internal/chaosgame/game/chaos_game_description.go
@@ -2,6 +2,7 @@ package game
 
 import (
 	"github.com/nokacper24/go-chaos-game/internal/chaosgame/model"
+	"github.com/nokacper24/go-chaos-game/internal/chaosgame/model/sign"
 	"github.com/nokacper24/go-chaos-game/internal/chaosgame/transform"
 )
 
@@ -25,6 +26,36 @@ var (
 			&transform.AffineTransform2D{
 				Matrix: model.Matrix2x2{A00: 0.5, A01: 0, A10: 0, A11: 0.5},
 				Vector: model.Vector2D{X: 0.25, Y: 0.5}},
+		},
+	}
+
+	BarnsleyFern = ChaosGameDescription{
+		MinCoords: model.Vector2D{X: 0, Y: 0},
+		MaxCoords: model.Vector2D{X: 1, Y: 1},
+		Transforms: []transform.Transform2D{
+			&transform.AffineTransform2D{
+				Matrix: model.Matrix2x2{A00: 0, A01: 0, A10: 0, A11: 0.16},
+				Vector: model.Vector2D{X: 0, Y: 0}},
+			&transform.AffineTransform2D{
+				Matrix: model.Matrix2x2{A00: 0.85, A01: 0.04, A10: -0.04, A11: 0.85},
+				Vector: model.Vector2D{X: 0, Y: 1.6}},
+			&transform.AffineTransform2D{
+				Matrix: model.Matrix2x2{A00: 0.2, A01: -0.26, A10: 0.23, A11: 0.22},
+				Vector: model.Vector2D{X: 0, Y: 0.16}},
+			&transform.AffineTransform2D{
+				Matrix: model.Matrix2x2{A00: -0.15, A01: 0.28, A10: 0.26, A11: 0.24},
+				Vector: model.Vector2D{X: 0, Y: 0.44}},
+		},
+	}
+
+	JuliaTransform = ChaosGameDescription{
+		MinCoords: model.Vector2D{X: -1.6, Y: -1},
+		MaxCoords: model.Vector2D{X: 1.6, Y: 1},
+		Transforms: []transform.Transform2D{
+			&transform.JuliaTransform{
+				Point: model.Vector2D{X: -0.74543, Y: 0.11301},
+				Sign:  sign.MINUS,
+			},
 		},
 	}
 )
